74efba797542c5b6d7822f7585ecee96cc2eaa17
Kishan Vaghela <kishan.j.vaghela@gmail.com>
2017-03-14 16:23:19 +0530

    initial commit

---
+++ storybook/stories/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { storiesOf, action, linkTo } from '@kadira/react-native-storybook';
+
+import Button from './Button';
+import CenterView from './CenterView';
+import Welcome from './Welcome';
+
+storiesOf('Welcome', module)
+  .add('to Storybook', () => (
+    <Welcome showApp={linkTo('Button')}/>
+  ));
+
+storiesOf('Text View', module)
+    .add('test', () => (
+      <Text>Hello Text a</Text>
+    ));
+
+storiesOf('Button', module)
+  .addDecorator(getStory => (
+    <CenterView>{getStory()}</CenterView>
+  ))
+  .add('with text a', () => (
+    <Button onPress={action('clicked-text')}>
+      <Text>Hello Button a</Text>
+    </Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onPress={action('clicked-emoji')}>
+      <Text>😀 😎 👍 💯</Text>
+    </Button>
+  ));
